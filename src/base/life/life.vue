ab5b3c8813c749c7a2c22de5b271219eb977de00
copiner <wdaonngg@gmail.com>
2021-03-10 15:37:02 +0800

    vm

--- src/base/life/life.vue
+++ src/base/life/life.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div ref="p">
     <p>The life {{ message }} tips.</p>
   </div>
 </template>
@@ -36,7 +36,43 @@ export default {
         console.log("%c%s", "color:red","el     : " + this.$el); //已被初始化
         console.log(this.$el);
         console.log("%c%s", "color:red","data   : " + this.$data); //已被初始化
+        console.log(this.$data)
         console.log("%c%s", "color:red","message: " + this.message); //已被初始化
+
+        console.log("%c%s", "color:red","实例原型，实例原型的原型");
+
+        //vm
+        console.log(this)
+        //console.log(Object.getPrototypeOf(this))
+
+        //实例原型
+        var _this = Object.getPrototypeOf(this);
+
+        //实例原型的原型
+        console.log(Object.getPrototypeOf(_this));
+
+        // for(let k in this){
+        //   console.log(k)
+        // }
+
+        //$refs
+        console.log("%c%s", "color:red","refs: " + this.$refs);
+        console.log(this.$refs)
+
+        //$destory
+        //console.log(this.$destroy())
+
+        //$root
+        console.log("%c%s", "color:red","root: " + this.$root);
+        console.log(this.$root)
+
+        //parent
+        console.log("%c%s", "color:red","parent: " + this.$parent);
+        console.log(this.$parent)
+
+        //options
+        console.log("%c%s", "color:red","options: " + this.$options);
+        console.log(this.$options)
      },
      beforeUpdate: function () {
         console.group('beforeUpdate 更新前状态===============》');
